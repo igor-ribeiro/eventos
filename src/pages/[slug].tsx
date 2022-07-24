ec9f01b6383eece0d81d4859eb73c78fa260eaf4
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-07-24 17:44:46 -0300

    feat: add event hero with temp image

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,6 +1,7 @@
 import { trpc } from "@/utils/trpc";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
+import imageURL from "@/assets/image.jpg";
 
 const EventPage: NextPage = () => {
   const router = useRouter();
@@ -16,7 +17,22 @@ const EventPage: NextPage = () => {
     return null;
   }
 
-  return <h1 className="font-bold text-xl">Evento: {data.name}</h1>;
+  return (
+    <header
+      className="hero bg-base-200 min-h-[250px] bg-center"
+      style={{
+        backgroundImage: `url(${imageURL.src})`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-50"></div>
+      <div className="hero-content text-center text-white">
+        <div className="max-w-md">
+          <h1 className="font-bold text-3xl md:text-5xl mb-6">{data.name}</h1>
+          <p>{data.description}</p>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default EventPage;
