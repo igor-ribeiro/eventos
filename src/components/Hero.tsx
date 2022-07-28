4d440d99b74c2ccf4aea26d499c1469b21bb029f
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-07-28 17:54:09 -0300

    fix: dynamic tailwind classes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,24 @@
 import { FC, ReactNode } from "react";
 
+type Position = "start" | "center" | "end";
+
+const POSITION_CLASSES: Record<Position, string> = {
+  start: "place-items-start",
+  end: "place-items-end",
+  center: "place-items-center",
+};
+
 export const Hero: FC<{
   children: ReactNode;
   image?: string | null;
   fullScreen?: boolean;
-  position?: "start" | "center" | "end";
+  position?: Position;
 }> = ({ image, fullScreen, position = "center", children }) => {
   return (
     <header
-      className={`hero bg-base-200 min-h-[200px] bg-center place-items-${position} ${
-        fullScreen ? "h-screen" : "h-[50vh]"
-      }`}
+      className={`hero bg-base-200 min-h-[200px] bg-center ${
+        POSITION_CLASSES[position]
+      } ${fullScreen ? "h-screen" : "h-[50vh]"}`}
       style={{
         backgroundImage: image ? `url(${image})` : undefined,
       }}
