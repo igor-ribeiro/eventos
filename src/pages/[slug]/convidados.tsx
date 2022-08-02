cfe3762803bdd10958a8c51e53c18969b35a15e1
Igor Ribeiro <igor.ribeiro.plus@gmail.com>
2022-08-02 01:00:48 -0300

    refactor: icons

--- src/pages/[slug]/convidados.tsx
+++ src/pages/[slug]/convidados.tsx
@@ -12,6 +12,7 @@ import removeAccents from "remove-accents";
 import { authOptions } from "../api/auth/[...nextauth]";
 import { appRouter } from "@/server/router";
 import { prisma } from "@/server/db/client";
+import { ClearIcon, DownloadIcon, RemoveIcon } from "@/components/Icons";
 
 type Total = Record<
   GuestConfirmation,
@@ -358,82 +359,6 @@ const CONFIRMATION_TEXT: Record<GuestConfirmation, string> = {
 
 function renderConfirmation(confirmation: GuestConfirmation): string {
   return CONFIRMATION_TEXT[confirmation];
-}
-
-function RemoveIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-      />
-    </svg>
-  );
-}
-
-function DownloadIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
-      />
-    </svg>
-  );
-}
-
-function ClearIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-      />
-    </svg>
-  );
-}
-
-function ExportIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth={2}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"
-      />
-    </svg>
-  );
 }
 
 export default ListPage;
